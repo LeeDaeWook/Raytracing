54102196b9db3a4e86e56355af60de71f2fb8553
LDW <leehan02@hanyang.ac.kr>
2023-05-09 15:23:24 +0900

    Adding 6.2

--- main.cpp
+++ main.cpp
@@ -4,14 +4,14 @@
 
 double	hit_sphere(const point3& center, double radius, const ray& r) {
 	vec3 oc = r.origin() - center;
-	auto a = dot(r.direction(), r.direction()); // getting length^2 of vector by operating dot product with itself.
-	auto b = 2.0 * dot(oc, r.direction());
-	auto c = dot(oc, oc) - radius*radius;
-	auto discriminant = b*b - 4*a*c;
+	auto a = r.direction().length_squared(); // getting length^2 of vector by operating dot product with itself.
+	auto half_b = dot(oc, r.direction());
+	auto c = oc.length_squared() - radius*radius;
+	auto discriminant = half_b*half_b - a*c;
 	if (discriminant < 0)
 		return -1.0;
 	else
-		return (-b - sqrt(discriminant)) / (2.0*a); // why only "-b - sqrt(discriminant)" ??
+		return (-half_b - sqrt(discriminant)) / a;
 }
 
 color ray_color(const ray& r) {
