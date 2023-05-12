94037a926e8fe8ab10a3f84fba563c31394a78a1
Daewook Lee <daewoole@c3r4s7.42seoul.kr>
2023-05-12 18:06:52 +0900

    Done to 8.2

--- main.cpp
+++ main.cpp
@@ -3,24 +3,20 @@
 #include "color.hpp"
 #include "hittable_list.hpp"
 #include "sphere.hpp"
+#include "camera.hpp"
 
-// double	hit_sphere(const point3& center, double radius, const ray& r) {
-// 	vec3 oc = r.origin() - center;
-// 	auto a = r.direction().length_squared(); // getting length^2 of vector by operating dot product with itself.
-// 	auto half_b = dot(oc, r.direction());
-// 	auto c = oc.length_squared() - radius*radius;
-// 	auto discriminant = half_b*half_b - a*c;
-// 	if (discriminant < 0)
-// 		return -1.0;
-// 	else
-// 		return (-half_b - sqrt(discriminant)) / a;
-// }
-
-color ray_color(const ray& r, const hittable& world) {
+color ray_color(const ray& r, const hittable& world, int depth) {
 	hit_record rec;
+
+	// If we've exceeded the ray bounce limit, no more light is gathered.
+	if (depth <= 0)
+		return color(0,0,0);
+
 	if (world.hit(r, 0, infinity, rec)) {
-		return 0.5 * (rec.normal + color(1,1,1));
+		point3 target = rec.p + rec.normal + random_in_unit_sphere();
+		return 0.5 * ray_color(ray(rec.p, target - rec.p), world, depth-1);
 	}
+
 	vec3 unit_direction = unit_vector(r.direction());
 	auto t = 0.5 * (unit_direction.y() + 1.0);
 	return (1.0-t)*color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 1.0);
@@ -32,6 +28,8 @@ int main()
 	const auto aspect_ratio = 16.0 / 9.0;
 	const int image_width = 400;
 	const int image_height = static_cast<int>(image_width / aspect_ratio);
+	const int samples_per_pixel = 100;
+	const int max_depth = 50;
 
 	// World
 	hittable_list world;
@@ -40,14 +38,7 @@ int main()
 
 	// Camera
 
-	auto viewport_height = 2.0;
-	auto viewport_width = aspect_ratio * viewport_height;
-	auto focal_length = 1.0;
-
-	auto origin = point3(0,0,0);
-	auto horizontal = vec3(viewport_width, 0, 0);
-	auto vertical = vec3(0, viewport_height, 0);
-	auto lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
+	camera cam;
 
 	// Render
 
@@ -57,12 +48,15 @@ int main()
 	{
 		std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
 		for (int i = 0; i < image_width; ++i)
-		{
-			auto u = double(i) / (image_width-1);
-			auto v = double(j) / (image_height-1);
-			ray r(origin, lower_left_corner + u*horizontal + v*vertical - origin);
-			color pixel_color = ray_color(r, world);
-			write_color(std::cout, pixel_color);
+		{	
+			color pixel_color(0,0,0);
+			for (int s = 0; s < samples_per_pixel; ++s) { // redering multisamples in a pixel
+				auto u = (i + random_double()) / (image_width-1);
+				auto v = (j + random_double()) / (image_height-1);
+				ray r = cam.get_ray(u, v);
+				pixel_color += ray_color(r, world, max_depth);
+			}
+			write_color(std::cout, pixel_color, samples_per_pixel);
 		}
 	}
 	std::cerr << "\nDone.\n";
